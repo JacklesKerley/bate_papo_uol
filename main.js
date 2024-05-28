0bc7a2a9509d1e96c40ba225bfbfc274cc94ec5e
Jackles Kerley <jackleskerley@yahoo.com.br>
2024-05-28 11:25:11 -0300

    feat: adicionar busca, render de participantes

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
+let UUID = '7b0bb360-7357-4f27-8c13-474005eccd8e'
+let destinatario = 'todos'
+
+//---------- Entrada de participante ----------
 let usuario = {}
 
 function nomeUsuario() {
-  let nome = prompt('Digite seu nome:')
-  usuario = {name: nome}
-  entrarSala()
+    let nome = prompt('Digite seu nome:')
+    usuario = {name: nome}
+    entrarSala()
 }
-
-nomeUsuario()
 
 function entrarSala() {
   axios
-    .post("https://mock-api.driven.com.br/api/v6/uol/participants/7b0bb360-7357-4f27-8c13-474005eccd8e", usuario)
+    .post("https://mock-api.driven.com.br/api/v6/uol/participants/" + UUID, usuario)
     .then(response => {
       console.log('conectado')
       buscarMensagens()
+      buscarParticipantes()
     })
     .catch(error => {
       alert('Nome de usuário existente, escolha outro!')
@@ -21,36 +24,113 @@ function entrarSala() {
     })
 }
 
-//Manter conectado
 setInterval(() => {
   axios
-    .post('https://mock-api.driven.com.br/api/v6/uol/status/7b0bb360-7357-4f27-8c13-474005eccd8e', usuario)
+    .post('https://mock-api.driven.com.br/api/v6/uol/status/' + UUID, usuario)
     .then(response => console.log('mantendo conectado'))
     .catch(function (error) {
       console.error('Erro ao obter os dados:', error)
     })
 }, 5000);
 
-//Busca mensagens
+
+// ---------- Lista de Participantes ----------
+let renderParticipantes = document.querySelector('.listaDeUsuarios')
+let listaDeParticipantes = ["todos"]
+
+function buscarParticipantes() {
+  axios
+    .get('https://mock-api.driven.com.br/api/v6/uol/participants/' + UUID)
+    .then(response => {
+      arrayDeParticipantes(response.data)
+    })
+    .catch(error => console.log('erro', error))
+}
+
+function arrayDeParticipantes(res) {
+  res.forEach(element => {
+    listaDeParticipantes.push(element.name)
+  })
+  renderListaDeParticipantes()
+}
+
+function renderListaDeParticipantes() {
+  let participantes = ''
+  for (let i = 0; i < listaDeParticipantes.length; i++) {
+    if (listaDeParticipantes[i].toLowerCase() === 'todos') {
+      participantes += `
+        <li onclick="selecionarParticipante(this)">
+        <ion-icon name="people"></ion-icon>
+          <p>${listaDeParticipantes[i]}</p>
+          <ion-icon name="checkmark-sharp" class="checkmark" id="${listaDeParticipantes[i]}"></ion-icon>
+        </li>
+      `
+    } else {
+      participantes += `
+        <li onclick="selecionarParticipante(this)">
+          <ion-icon name="person-circle"></ion-icon>
+          <p>${listaDeParticipantes[i]}</p>
+          <ion-icon name="checkmark-sharp" class="checkmark" id="${listaDeParticipantes[i]}"></ion-icon>
+        </li>
+      `
+    }
+  }
+
+  renderParticipantes.innerHTML = participantes
+
+  if (listaDeParticipantes.includes(destinatario)) {
+    definirSelecionando(destinatario)
+  } else {
+    definirSelecionando('todos')
+  }
+
+  listaDeParticipantes = ["todos"]
+}
+
+function definirSelecionando(i) {
+  let selecionado = document.querySelector(`#${i}`)
+  console.log(selecionado)
+  if (selecionado) {
+    selecionado.classList.toggle('visible')
+  }
+}
+
+setInterval(() => {
+  buscarParticipantes()
+}, 10000)
+
+// ---------- Lista de Mensagens  ----------
+let mensagemRenderizada = document.querySelector('.conteudo')
+let mensagens = ''
+
 function buscarMensagens() {
   axios
-    .get('https://mock-api.driven.com.br/api/v6/uol/messages/7b0bb360-7357-4f27-8c13-474005eccd8e')
+    .get('https://mock-api.driven.com.br/api/v6/uol/messages/' + UUID)
     .then(response => renderMensagens(response.data))
     .catch(error => console.log(error))
 }
 
-//renderiza mensagens
 function renderMensagens(res) {
-  let mensagemRenderizada = document.querySelector('.conteudo')
-
-  let mensagens = ''
-
   res.forEach(element => {
-    mensagens += `
-      <div class="divMensagem entrada">
-        <p class="mensagem"><span class="data">(${element.time})</span> <span>${element.from}</span> ${element.text}</p>
-      </div>
-    `
+    if (element.type === 'status') {
+      mensagens += `
+        <div class="divMensagem entrada">
+          <p class="mensagem"><span class="data">(${element.time})</span> <span>${element.from}</span> ${element.text}</p>
+        </div>
+      `
+    } else if (element.type === 'message'){
+      mensagens += `
+        <div class="divMensagem">
+          <p class="mensagem"><span class="data">(${element.time})</span> <span>${element.from}</span> para <span>${element.to}</span>: ${element.text}</p>
+        </div>
+      `
+    } else {
+      mensagens += `
+        <div class="divMensagem reservado">
+          <p class="mensagem"><span class="data">(${element.time})</span> <span>${element.from}</span> reservadamente para <span>${element.to}</span>: ${element.text}</p>
+        </div>
+      `
+    }
   });
    
   mensagemRenderizada.innerHTML = mensagens
@@ -61,7 +141,59 @@ function renderMensagens(res) {
 
 setInterval(() => {
   buscarMensagens()
-}, 3000);
+}, 3000)
+
+
+
+
+
+
+
+
+//---------------------
+
+
+
+
+
+
+function selecionarParticipante(participante) {
+  let selecionado = document.querySelector('.checkmark.visible')
+  let btnCheck = participante.querySelector('.checkmark')
+
+  btnCheck.classList.toggle('visible')
+  selecionado.classList.toggle('visible')
+
+  destinatario = participante.querySelector('p').textContent
+  
+  console.log(participante)
+  console.log(destinatario)
+}
+
+//Enviar mensagem
+
+
+function enviarMensagem() {
+  let mensagem = {
+    from: usuario.name,
+    to: destinatario,
+    text: "mensagem teste 5",
+    type: "private_message"
+  }
+
+  axios
+    .post('https://mock-api.driven.com.br/api/v6/uol/messages/' + UUID, mensagem)
+    .then(response => {
+      buscarMensagens()
+      console.log('mensagem enviada')
+    })
+    .catch(error => console.log('mensagem nao enviada'))
+}
+
+// setInterval(() => {
+//   enviarMensagem()
+// }, 6000);
+
 
 
 
@@ -77,4 +209,5 @@ menuBtn.addEventListener('click', function (event) {
 })
 
 
-
+//---------- Funções de inicialização ----------
+nomeUsuario()
